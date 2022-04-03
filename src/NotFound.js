d2a29f22bb8ae74974d2df3fd4ca602c60bf6825
Rylan Schubkegel <rylanschubkegel@gmail.com>
2022-04-02 19:36:56 -0700

    Stylize 404 page

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,7 +1,10 @@
 function NotFound() {
     return (
-        <div className="wrapper">
-            <h1>404: PAGE NOT FOUND</h1>
+        <div className="wrapper" style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+            <div style={{width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'var(--yellow)'}}>
+                <h1 style={{color: 'var(--dark-slate)'}}>404: PAGE NOT FOUND</h1>
+            </div>
+            <img src="logo.svg" style={{marginTop: '30vh', width: '8rem', height: '8rem'}}/>
         </div>
     );
 }
