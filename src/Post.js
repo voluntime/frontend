d9604d9faa00e1eed16ffc91f8ad2266b0b861a3
Adam Hearn <22334119+hearnadam@users.noreply.github.com>
2022-04-02 20:50:52 -0700

    Please God, help this work

--- src/Post.js
+++ src/Post.js
@@ -5,37 +5,85 @@ import React, { useCallback } from 'react';
 import {useNavigate} from 'react-router-dom';
 
 function Post() {
+    const [organization, setOrganization] = useState();
+    const [title, setTitle] = useState();
+    const [type, setType] = useState();
+    const [body, setBody] = useState();
+    const [eventLocation, setEventLocation] = useState();
+    const [volunteerGoal, setvolunteerGoal] = useState();
+    const [beginsAt, setBeginsAt] = useState();
+    const [endsAt, setEndsAt] = useState();
+    const [errorMessage, setErrorMessage] = useState('');
+
+
     const navigate = useNavigate();
-    const handleClick = useCallback(() => navigate('/', {replace : true}), [navigate]);
+    const backToFeed = useCallback(() => navigate('/', {replace : true}), [navigate]);
+    const submitForm = async (postDetails) {
+        fetch("https://api.volunti.me/v1/post", {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(postDetails),
+            credentials: "include"
+            }).then(function(response) {
+              if (response.ok) {
+                  backToFeed()
+              } else {
+                  console.log('we are screwed');
+              }
+          });
+    }
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        await submitForm({
+            organization,
+            title,
+            type,
+            body,
+            eventLocation,
+            volunteerGoal,
+            beginsAt,
+            endsAt,
+        })
+    }
 
     return (
         <div className="wrapper">
             <Stack>
                 <Stack direction={"row"}>
-                    <Button onClick={handleClick}><DynamicFeedIcon /></Button>
+                    <Button onClick={handleSubmit}><DynamicFeedIcon /></Button>
                     <h2>new event</h2>
                 </Stack>
                 <div className="form">
                     <form>
                         <div className="input-container">
                             <label>Title</label>
-                            <input type="text" name="title" placeholder="" />
+                            <input type="text" name="title" onChange={e => setTitle(e.target.value)} placeholder="" />
                         </div>
                         <div className="input-container">
-                            <label>Location</label>
-                            <input type="text" name="location" placeholder="" />
+                            <label>Event Location</label>
+                            <input type="text" name="location" onChange={e => setEventLocation(e.target.value)} placeholder="" />
                         </div>
                         <div className="input-container">
                             <label>Type</label>
-                            <input type="text" name="type" placeholder="" />
+                            <input type="text" name="type" onChange={e => setType(e.target.value)} placeholder="" />
                         </div>
                         <div className="input-container">
                             <label>Volunteer Count</label>
-                            <input type="text" name="count" placeholder="" />
+                            <input type="text" name="count" onChange={e => setvolunteerGoal(e.target.value)} placeholder="" />
                         </div>
                         <div className="input-container">
-                            <label>Description</label>
-                            <input type="text" name="desc" placeholder="" />
+                            <label>Post Body</label>
+                            <input type="text" name="desc" onChange={e => setBody(e.target.value)} placeholder="" />
+                        </div>
+                        <div className="input-container">
+                            <label>Begins At</label>
+                            <input type="datetime-local" name="begins" onChange={e => setBeginsAt(e.target.value)} placeholder="" />
+                        </div>
+                        <div className="input-container">
+                            <label>Ends At</label>
+                            <input type="datetime-local" name="ends" onChange={e => setEndsAt(e.target.value)} placeholder="" />
                         </div>
                         <div className="button-container">
                             <Button type="submit" variant="contained" color="primary">Post</Button>
