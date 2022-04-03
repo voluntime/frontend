66ad96054aa9eb0aeab06b78725cb80c3e6a524b
Rylan Schubkegel <rylanschubkegel@gmail.com>
2022-04-02 18:58:26 -0700

    Add placeholder layout to settings

--- src/Settings.js
+++ src/Settings.js
@@ -1,8 +1,33 @@
+import { useNavigate } from "react-router-dom";
+import React, { useCallback } from "react";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
+
 function Settings() {
-    return (
-        <div className="wrapper">
-        </div>
-    );
+  const navigate = useNavigate();
+  const profileClicked = useCallback(
+    () => navigate("/profile", { replace: true }),
+    [navigate]
+  );
+  return (
+    <div className="wrapper">
+      <Stack alignItems={"center"}>
+        {/* HEADER */}
+        <Stack direction='row' className="profileHeader" justifyContent={'center'} alignItems={"center"} sx={{ width: '100%' }} >
+          <Stack direction='row' justifyContent={'space-between'} sx={{width: '100%', maxWidth: 'var(--content-width)'}}>
+            <Button onClick={profileClicked}>
+              <Avatar sx={{ width: "var(--avatar-size)", height: "var(--avatar-size)" }}>H</Avatar>
+            </Button>
+            <h2>settings</h2>
+          </Stack>
+        </Stack>
+
+        {/* SETTINGS */}
+        <p>Settings go here....</p>
+      </Stack>
+    </div>
+  );
 }
 
 export default Settings;
